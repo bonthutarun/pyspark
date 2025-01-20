1c7e6549a10542aa03e50dac513d1b801e4ce938
bonthu <bonthutarunteja123@gmail.com>
2025-01-20 14:43:50 +0530

    reverse order in array

---
+++ array4.py
@@ -0,0 +1,12 @@
+
+arr = [5, 10, 15, 20, 25]
+
+print("Array elements in reverse order:")
+
+i = len(arr) - 1
+while i >= 0:
+    print(arr[i])
+    i -= 1
+
+# for i in range(len(arr) - 1, -1, -1)
+# array[::-1]
